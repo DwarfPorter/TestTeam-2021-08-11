d934eddaa733bbe0617480a2a02b0b12d939ff3d
Vladimir Morozov <mrvlmor@gmail.com>
2021-08-11 21:54:56 +0300

    вывод сообщения (#3)

--- app/src/main/java/ru/geekbrains/testteam/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/testteam/MainActivity.kt
@@ -2,16 +2,17 @@ package ru.geekbrains.testteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    
-    val tv: TextView?
-    
+
+    var tv: TextView?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         tv = findViewById<TextView>(R.id.text_view)
-        textView.text = "MY String"
+        tv.text = "String"
     }
 }
